075a50481932e92a70229e46e6bf70e44e90056d
Jose <josefbautista94@gmail.com>
2025-03-30 21:19:57 -0400

    added an image to show that gets pulled from the api when you type a pokemons name

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ axios.defaults.baseURL = "https://pokeapi.co/api/v2/"
 
 const searchInput = document.getElementById("pokeSearch")
 const searchButton = document.getElementById("pokeButton")
+const displayDiv = document.getElementById("pokemonImageDiv")
 
 
 async function getPokiData(name){
 
     try{
         const res = await axios.get(`/pokemon/${name}`)
-        console.log(res.data)
+        const data = res.data;
+
+        console.log(data)
+
+        displayDiv.innerHTML = "" //Clearing Previous Pokemon
+        const pokeImg = document.createElement("img")
+            pokeImg.src = data.sprites.front_default;
+            pokeImg.alt = data.namel
+            pokeImg.style.width = "300px"
+            displayDiv.appendChild(pokeImg)
+        
     }
     catch(error){
 console.log(`There was an error : ${error}`)
